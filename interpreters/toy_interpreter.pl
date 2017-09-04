4660065d05b54e31b031e1ed62978d0aeb2b47b3
vinaybenny <vinaybenny@gmail.com>
2017-09-04 21:36:32 +1200

    Troubleshooting and fix for else statements not being executed. Also added WHILE loop functionality. This interpreter can be considered complete for now. The toy language now has basic arithmetic operations capabilities, if-else conditional branching, and looping based on while - do conditions.

--- interpreters/toy_interpreter.pl
+++ interpreters/toy_interpreter.pl
@@ -31,6 +31,8 @@ operators have precedence of 700 and below.
 */
 :- op(750,fx,if).
 :- op(749,xfx,then).
+:- op(750,fx,while).
+:- op(749,xfx,do).
 :- op(748,xfx,else).
 :- op(747,yf,;). /* Least precedence; statements in input program need to be first broken by semicolons */
 
@@ -51,18 +53,29 @@ interpret( (St;), Dictin, Dictout)  :- interpret(St, Dictin, Dictout).
 /* For statements of assignment type, strip out components of the statement, and create key-value pair
 for the assigned variable and value. put_assoc/4 is an autoloaded library in Prolog that enables inserting
 key value pairs into an association dictionary*/
-interpret( (Var=Expr), Dictin, Dictout):- atom(Var), evaluate(Expr, Val, Dictin), put_assoc(Var, Dictin, Val, Dictout).
-
-interpret( (if Condexpr then { St1 } ), Dictin, Dictout ):- evaluate(Condexpr, Condval, Dictin),
-    (   Condval #= 1
-    ->  interpret(St1, Dictin, Dictout)
-    ;   Dictout =  Dictin).
+interpret( (Var=Expr), Dictin, Dictout):- atom(Var), evaluate(Expr, Val, Dictin), put_assoc(Var, Dictin, Val, Dictout),!.
+/* For if-else statements, evaluate condition first. Based on value of condition being 1 or 0 (True/False),
+    evaluate statement 1 or statement 2 selectively.*/
 interpret( (if Condexpr then { St1 } else { St2 } ), Dictin, Dictout ):- evaluate(Condexpr, Condval, Dictin),
-    (   Condval #= 1
+    Check =.. [#=, Condval, 1],
+    (   Check
     ->  interpret(St1, Dictin, Dictout)
     ;   interpret(St2, Dictin, Dictout)).
-
-
+/* For if statements, evaluate condition first. Based on value of condition being 1 or 0 (True/False),
+    evaluate statement 1 or not.*/
+interpret( (if Condexpr then { St1 } ), Dictin, Dictout ):- evaluate(Condexpr, Condval, Dictin),
+    Check =.. [#=, Condval, 1],
+    (   Check
+    ->  interpret(St1, Dictin, Dictout)
+    ;   Dictout =  Dictin).
+/* For While-Do statements, evaluate condition using an if-else statement, and based on outcome,
+    execute body of while loop and recursively call while-do statement again. If the if-else condtion is
+    false, execute the else statement, which is a dummy statement.*/
+interpret(while Condexpr do {St}, Dictin, Dictout ):-
+    interpret(if Condexpr then {
+                              St;
+                              while Condexpr do {St}
+                          }, Dictin, Dictout).
 
 /*******************************************************************************************************
 Evaluate/3: These functions will evaluate the arithmetic expressions in
@@ -72,10 +85,10 @@ the integer value of that variable is obtained from the dictionary, and
 used to calculate the value represented by the variable.
 *******************************************************************************************************/
 /* If the expression is just an integer, the value is just that integer.*/
-evaluate(Intval, Intval, _):- integer(Intval).
+evaluate(Intval, Intval, _):- integer(Intval),!.
 /* If the expression is a variable, obtain the value of that variable from the dictionary.
 get_assoc/3 is an autoloaded function that takes in a key, and returns its value pair.*/
-evaluate(Var, Val, Dictin):- atom(Var), get_assoc(Var, Dictin, Val).
+evaluate(Var, Val, Dictin):- atom(Var), get_assoc(Var, Dictin, Val),!.
 /* If the expression is an arithmetic operation, break it into left and right operands and
 the operator. Evaluate each operand, obtain its value and use the values to create the final
 value of the expression.*/
@@ -83,22 +96,20 @@ evaluate(Expr, Val, Dictin):- Expr =.. [Op, Left, Right],
     member(Op, [+, -, *, //]),
     evaluate(Left, Partval1, Dictin), evaluate(Right, Partval2, Dictin),
     Result =.. [Op,Partval1, Partval2],
-    Val #= Result.
+    Val #= Result,!.
 /* Handle uni-operand arithmetic operations, like cases where a number is specified as a
 a double negative or a negative-positive number is specified. eg: b = 1+-2*/
 evaluate(Expr, Val, Dictin):- Expr =..[Op, Right],
     member(Op,[+, -]),
     evaluate(Right, Partval, Dictin),
     Result =.. [Op, Partval],
-    Val #= Result.
+    Val #= Result,!.
 /* Handle conditional operators where the Expr evaluates to True =1/False=0. */
 evaluate(Expr, Val, Dictin):- Expr =.. [Op, Left, Right],
     member(Op,[<, =<, >, >=, =, \=]),
     evaluate(Left, Partval1, Dictin), evaluate(Right, Partval2, Dictin),
     atomic_concat('#',Op, Newop), /*  Concatenates # to operator to create a clpfd comparison operator*/
-    Result =.. [Newop, Partval1, Partval2],
-    ( Result -> Val = 1; Val = 0 ).
+    Result =.. [Newop, Partval1, Partval2],( Result -> Val = 1; Val = 0 ),!.
 
 /*Test Program*/
-
-:-Program = ( a = 24 ; b = a*2 ; if b == 47 then {c = b + -5} else { c = 4} ; ), empty_assoc(XX), interpret(Program, XX, YY).
+/*Program = ( a = 10 ; if a > 10 then {c = 5} else { c = 4} ; while c < 10 do { c = c+1} ; ),interpret(Program, XX, YY).*/
